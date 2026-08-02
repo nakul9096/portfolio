4fd8e0846d4d25faf648fa1c5534cf83e69a7621
Nakul Dhande <148444171+nakul9096@users.noreply.github.com>
2026-08-02 16:18:21 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,13 @@
 window.CONFIG = {
   SUPABASE_URL: "https://vmorzlhqeeosmwkjfumm.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3J6bGhxZWVvc213a2pmdW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2MDAyMTEsImV4cCI6MjEwMTE3NjIxMX0.m1tmsh4u9wtY5PcMo0BrRrVeA0SOGibyhV7yIXDN7Gg"
+  FIREBASE_CONFIG: {
+      apiKey: "AIzaSyBR1L9UACOdMfOHaOPerfZY1SyzPEBnEGA",
+  authDomain: "myportfolio-9096.firebaseapp.com",
+  projectId: "myportfolio-9096",
+  storageBucket: "myportfolio-9096.firebasestorage.app",
+  messagingSenderId: "1614082917",
+  appId: "1:1614082917:web:e844f4b92091986c4be78c",
+  measurementId: "G-6E9VPG1QYM"
+    }
 };
