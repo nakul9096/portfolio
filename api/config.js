4116cab07c7cc8061e580164da481d134e1bdc4d
Nakul Dhande <148444171+nakul9096@users.noreply.github.com>
2026-08-02 15:58:23 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
-  const SUPABASE_URL = "https://vmorzlhqeeosmwkjfumm.supabase.co",
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3J6bGhxZWVvc213a2pmdW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2MDAyMTEsImV4cCI6MjEwMTE3NjIxMX0.m1tmsh4u9wtY5PcMo0BrRrVeA0SOGibyhV7yIXDN7Gg"
+  const SUPABASE_URL: "https://vmorzlhqeeosmwkjfumm.supabase.co",
+  const SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3J6bGhxZWVvc213a2pmdW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2MDAyMTEsImV4cCI6MjEwMTE3NjIxMX0.m1tmsh4u9wtY5PcMo0BrRrVeA0SOGibyhV7yIXDN7Gg"
 };
