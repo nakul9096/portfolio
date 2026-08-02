49c294c9b904f74389544add70621a3f50165593
Nakul Dhande <148444171+nakul9096@users.noreply.github.com>
2026-08-02 16:09:17 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+  SUPABASE_URL: "https://vmorzlhqeeosmwkjfumm.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3J6bGhxZWVvc213a2pmdW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2MDAyMTEsImV4cCI6MjEwMTE3NjIxMX0.m1tmsh4u9wtY5PcMo0BrRrVeA0SOGibyhV7yIXDN7Gg"
+};
